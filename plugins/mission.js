8cd0592a94d7ac3ead66e1daaf9c2fec0e80b867
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-03-24 14:23:31 +0300

    Update mission.js

--- plugins/mission.js
+++ plugins/mission.js
@@ -19,7 +19,7 @@ let handler = async (m, {conn, usedPrefix, usedPrefix: _p, __dirname, text, isPr
     let uptime = clockString(_uptime)
 let who = m.quoted ? m.quoted.sender : m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 if (!(who in global.db.data.users)) throw `✳️ The user is not found in my database`
-let videoUrl = 'https://telegra.ph/file/2be243f360c2b81977f51.jpg';
+let videoUrl = 'https://telegra.ph/file/40d1ce62142be61a61c4c.mp4';
   let vn = './media/menu.mp3';
   const user = global.db.data.users[m.sender];
   const {money, joincount} = global.db.data.users[m.sender];
