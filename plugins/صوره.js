4479ec54af29c472e0539f664086b67c0f2b37a9
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-03-25 07:44:37 +0300

    Update صوره.js

--- plugins/صوره.js
+++ plugins/صوره.js
@@ -1,6 +1,6 @@
 import { googleImage } from '@bochilteam/scraper'
 let handler = async (m, { conn, text, usedPrefix, command }) => {
-if (!text) throw `*[❗خطاء❗] مثال علي الامر ${usedPrefix + command} كانيكي*`
+if (!text) throw `*[❗خطاء❗] مثال علي الامر ${usedPrefix + command} كاتاكوري*`
 if (m.text.includes('gore') || m.text.includes('cp')|| m.text.includes('porno')|| m.text.includes('Gore')|| m.text.includes('rule')|| m.text.includes('CP')|| m.text.includes('Rule34')) return m.reply('[❗خطاء❗] لا يمكنني إرسال هذا المحتوى ، المجموعة محظورة \n إذا كنت مشرفًا وتريد تنشيطها ، اخبر المطور')  
 const res = await googleImage(text)
 let image = await res.getRandom()
