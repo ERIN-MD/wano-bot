7ca3946c62dd1df0b7186baec950308a35d4f596
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-03-25 06:54:00 +0300

    Update antiprivate.js

--- plugins/antiprivate.js
+++ plugins/antiprivate.js
@@ -6,7 +6,7 @@ export async function before(m, {conn, isAdmin, isBotAdmin, isOwner, isROwner}) 
   const chat = global.db.data.chats[m.chat];
   const bot = global.db.data.settings[this.user.jid] || {};
   if (bot.antiPrivate && !isOwner && !isROwner) {
-    await m.reply(`*[❗] ʜᴏʟᴀ @${m.sender.split`@`[0]}, كلم المطور لرفع لحظر عنكك او ادخل جروب دعم رسمي للبوت المطور: +22237979043 يمنع التحدث بالرسائل الخاصة للبوت وبالتالي سيتم حظرك.*`, false, {mentions: [m.sender]});
+    await m.reply(`*[❗] ʜᴏʟᴀ @${m.sender.split`@`[0]}, كلم المطور لرفع لحظر عنكك او ادخل جروب دعم رسمي للبوت المطور: +967730068059 يمنع التحدث بالرسائل الخاصة للبوت وبالتالي سيتم حظرك.*`, false, {mentions: [m.sender]});
     await this.updateBlockStatus(m.chat, 'block');
   }
   return !1;
