626df0447d4dabbe271d47fc7cbf9cbad7798e1d
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-03-25 03:26:20 +0300

    Update نرد.js

--- plugins/نرد/نرد.js
+++ plugins/نرد/نرد.js
@@ -5,10 +5,6 @@ const dir = [
   'https://raw.githubusercontent.com/Dark-Man747/worker-bot/main/dado/dado4.webp',
   'https://raw.githubusercontent.com/Dark-Man747/worker-bot/main/dado/dado5.webp',
   'https://raw.githubusercontent.com/Dark-Man747/worker-bot/main/dado/dado6.webp'
-];
-let handler = async (m, { conn }) => {
-  conn.sendFile(m.chat, dir[Math.floor(Math.random(🎲) * dir.length)], 'dado.webp', 'كاتاكوري', m)
-  m.react('🎲')
 }
 handler.help = ['dado']
 handler.tags = ['game']
