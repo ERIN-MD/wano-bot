bc8f4694d6336ccac7071c7e396224a073a9714a
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-03-26 15:16:02 +0300

    Update دمج.js

--- plugins/دمج.js
+++ plugins/دمج.js
@@ -1,25 +1,28 @@
-import { sticker } from '../lib/sticker.js'
-import MessageType from '@adiwajshing/baileys'
-import fetch from 'node-fetch'
-import fs from "fs"
-let handler = async (m, { conn, text, args }) => {
-if (!args[0]) throw '*باستخدام هذا الأمريجب ان يكون #دمج <ايموجي 1>+<ايموجي 2>*\n*مثال:*\n*.دمج 🤨+😣*'
-let [emoji1, emoji2] = text.split`+`
-let anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`)
-for (let res of anu.results) {
-let stiker = await sticker(false, res.url, global.packname, global.author)
-conn.sendFile(m.chat, stiker, null, { asSticker: true })
-}}
-handler.help = ['دمج'].map(v => v + ' emot1|emot2>')
-handler.tags = ['fun']
-handler.command = /^(دمج)$/i
-export default handler
+import {sticker} from '../lib/sticker.js';
+import MessageType from '@whiskeysockets/baileys';
+import fetch from 'node-fetch';
+import fs from 'fs';
+const handler = async (m, {conn, text, args}) => {
+  if (!args[0]) throw '*[💠مساعده💠] قم باختيار اتنين من الايمواجي ليتم دمجهم*\n*مثل:*\n*.دمج 🤨+😣*';
+  const [emoji1, emoji2] = text.split`+`;
+  const anu = await fetchJson(`https://tenor.googleapis.com/v2/featured?key=AIzaSyAyimkuYQYF_FXVALexPuGQctUWRURdCYQ&contentfilter=high&media_filter=png_transparent&component=proactive&collection=emoji_kitchen_v5&q=${encodeURIComponent(emoji1)}_${encodeURIComponent(emoji2)}`);
+  for (const res of anu.results) {
+    const stiker = await sticker(false, res.url, global.packname, global.author);
+    conn.sendFile(m.chat, stiker, null, {asSticker: true});
+  }
+};
+handler.help = ['emojimix'].map((v) => v + ' emot1|emot2>');
+handler.tags = ['fun'];
+handler.command = /^(دمج)$/i;
+handler.limit = 2
+export default handler;
 const fetchJson = (url, options) => new Promise(async (resolve, reject) => {
-fetch(url, options)
-.then(response => response.json())
-.then(json => {
-resolve(json)
-})
-.catch((err) => {
-reject(err)
-})})
+  fetch(url, options)
+      .then((response) => response.json())
+      .then((json) => {
+        resolve(json);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+});
