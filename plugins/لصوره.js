6798f695eff2a38218982ace61d82bb4453c80e1
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-03-26 13:35:48 +0300

    Update لصوره.js

--- plugins/لصوره.js
+++ plugins/لصوره.js
@@ -1,15 +1,15 @@
-import { webp2png } from '../lib/webp2mp4.js'
-let handler = async (m, { conn, usedPrefix, command }) => {
-const notStickerMessage = `*رد علي الملصق الذي تريد تحويله لصوره ${usedPrefix + command}*`
-if (!m.quoted) throw notStickerMessage
-const q = m.quoted || m
-let mime = q.mediaType || ''
-if (!/sticker/.test(mime)) throw notStickerMessage
-let media = await q.download()
-let out = await webp2png(media).catch(_ => null) || Buffer.alloc(0)
-await conn.sendFile(m.chat, out, 'error.png', null, m)
-}
-handler.help = ['toimg (reply)']
-handler.tags = ['sticker']
-handler.command = ['لصوره', 'صورة', 'لصورة']
-export default handler
+import {webp2png} from '../lib/webp2mp4.js';
+const handler = async (m, {conn, usedPrefix, command}) => {
+  const notStickerMessage = `*««لـصـوره»»*\n قم بالرد علي الملصق اللي عايز تحولوا لصوره*`;
+  if (!m.quoted) throw notStickerMessage;
+  const q = m.quoted || m;
+  const mime = q.mediaType || '';
+  if (!/sticker/.test(mime)) throw notStickerMessage;
+  const media = await q.download();
+  const out = await webp2png(media).catch((_) => null) || Buffer.alloc(0);
+  await conn.sendFile(m.chat, out, 'error.png', null, m);
+};
+handler.help = ['toimg (reply)'];
+handler.tags = ['sticker'];
+handler.command = ['لصوره', 'لصورة', 'img'];
+export default handler;
