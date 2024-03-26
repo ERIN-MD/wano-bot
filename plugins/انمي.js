0d021d2f1d8001ab28af5954a204b3e3240a248a
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-03-27 01:21:52 +0300

    Update انمي.js

--- plugins/انمي.js
+++ plugins/انمي.js
@@ -1,33 +1,40 @@
-import translate from '@vitalets/google-translate-api'
-import { Anime } from "@shineiichijo/marika"
+import {translate} from '@vitalets/google-translate-api';
+import { Anime } from '@shineiichijo/marika';
+
 const client = new Anime();
-let handler = async(m, { conn, text, usedPrefix }) => {
-if (!text) return m.reply(`*[❗] حط اسم الانمي ال انت عايز تدور عليه*`)
-try {  
-let anime = await client.searchAnime(text)
-let result = anime.data[0];
-let resultes = await translate(`${result.background}`, { to: 'es', autoCorrect: true })   
-let resultes2 = await translate(`${result.synopsis}`, { to: 'es', autoCorrect: true })   
-let AnimeInfo = `
-🎀 • *الاسم:* ${result.title}
-🎋 • *شكل:* ${result.type}
-📈 • *ولاية:* ${result.status.toUpperCase().replace(/\_/g, " ")}
-🍥 • *عدد الحلقات:* ${result.episodes}
-🎈 • *مدة: ${result.duration}*
-✨ • *مقتبس من:* ${result.source.toUpperCase()}
-💫 • *اول عرض:* ${result.aired.from}
-🎗 • *تم الانتهاء:* ${result.aired.to}
-🎐 • *الشعبية:* ${result.popularity}
+
+let handler = async (m, { conn, text, usedPrefix }) => {
+  if (!text) return m.reply(`*[❗] يرجى إدخال اسم أنمي للبحث.*`);
+  try {
+    let anime = await client.searchAnime(text);
+    let result = anime.data[0];
+    let resultes = await translate(`${result.background}`, { to: 'ar', autoCorrect: true });
+    let resultes2 = await translate(`${result.synopsis}`, { to: 'ar', autoCorrect: true });
+    let AnimeInfo = `
+🎀 • *العنوان:* ${result.title}
+🎋 • *الصيغة:* ${result.type}
+📈 • *الحالة:* ${result.status.toUpperCase().replace(/\_/g, ' ')}
+🍥 • *إجمالي الحلقات:* ${result.episodes}
+🎈 • *المدة:* ${result.duration}*
+✨ • *مستند إلى:* ${result.source.toUpperCase()}
+💫 • *تاريخ الإصدار:* ${result.aired.from}
+🎗 • *انتهى في:* ${result.aired.to}
+🎐 • *الشهرة:* ${result.popularity}
 🎏 • *المفضلة:* ${result.favorites}
-🎇 • *التصنيف:* ${result.rating}
-🏅 • *المركز:* ${result.rank}
-♦ • *التيلر:* ${result.trailer.url}
-🌐 • *ع انمي ليست:* ${result.url}
-🎆 • *لمحة:* ${resultes.text}
-❄ • *سيرة:* ${resultes2.text}`
-conn.sendFile(m.chat, result.images.jpg.image_url, 'error.jpg', AnimeInfo, m)
-} catch {
-throw `*[❗] خطأ، حاول مرة أخرى*`  
-}}
-handler.command = /^(anime|انمي)$/i
-export default handler 
+🎇 • *التقييم:* ${result.rating}
+🏅 • *التصنيف:* ${result.rank}
+♦ • *التريلر:* ${result.trailer.url}
+🌐 • *رابط الأنمي:* ${result.url}
+🎆 • *الخلفية:* ${resultes.text}
+❄ • *الملخص:* ${resultes2.text}`;
+
+    conn.sendFile(m.chat, result.images.jpg.image_url, 'error.jpg', AnimeInfo, m);
+  } catch {
+    throw `*[❗] خطأ، يرجى المحاولة مرة أخرى.*`;
+  }
+};
+
+handler.help = ['anime']
+handler.tags = ['anime']
+handler.command = /^(انمي|animeinfo)$/i;
+export default handler;
