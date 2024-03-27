ec43e680b2a9f6e2852e8678e0a5a61534556c85
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-03-27 22:50:00 +0300

    Create ايتشيغو.js

---
+++ plugins/let handler = m => m;      handler.all = async function (m) {     let chat = global.db.data.chats[m.chat];     let responses;     if (/^كت$/ايتشيغو.js
@@ -0,0 +1,19 @@
+let handler = m => m; 
+  
+ handler.all = async function (m) { 
+   let chat = global.db.data.chats[m.chat]; 
+   let responses; 
+   if (/^ايتشيغو$/i.test(m.text)) { 
+     responses = [ 
+`ورع وانو`
+ 
+      ];
+    }  
+   if (responses) { 
+     let randomIndex = Math.floor(Math.random() * responses.length); 
+     conn.reply(m.chat, responses[randomIndex], m); 
+   } 
+   return !0 
+ }; 
+  
+ export default handler;
