758200c2525ca08ddf9786d7b42d42e41781c081
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-03-28 18:56:56 +0300

    Update handler.js

--- handler.js
+++ handler.js
@@ -1547,7 +1547,7 @@ global.dfail = (type, m, conn) => {
     restrict: '*[ ℹ️ ] تم تعطيل هذا الأمر من قبل مالك الروبوت.*',
   }[type];
   const aa = {quoted: m, userJid: conn.user.jid};
-  const prep = generateWAMessageFromContent(m.chat, {extendedTextMessage: {text: msg, contextInfo: {externalAdReply: {title: '*[ ⚠ ] Warning*', body: 'JOHAN-BOT', thumbnail: imagen1, sourceUrl: 'https://chat.whatsapp.com/CjPwuDRKkUBQut8Pfkla'}}}}, aa);
+  const prep = generateWAMessageFromContent(m.chat, {extendedTextMessage: {text: msg, contextInfo: {externalAdReply: {title: '*[ ⚠ ] Warning*', body: '⛩️┃🏮WONO🏮┃⛩️', thumbnail: imagen1, sourceUrl: 'https://chat.whatsapp.com/F8nL19lEuWmL4xleej7PsT'}}}}, aa);
   if (msg) return conn.relayMessage(m.chat, prep.message, {messageId: prep.key.id});
 };
 
