02addf9ad4f30df7796d5db3974127017cb3636a
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-03-28 03:54:46 +0300

    Update الي يجي خ.js

--- plugins/الي يجي خ.js
+++ plugins/الي يجي خ.js
@@ -2,7 +2,7 @@ export async function all(m) {
 	
   // when someone sends a group link to the bot's dm
   if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('https://chat') || m.text.startsWith('افتح هذا الرابط')) && !m.isBaileys && !m.isGroup) {
-   this.sendMessage(m.chat,{text:`مرحبًا @${m.sender.split('@')[0]}\nيمكنك استئجار الروبوت للانضمام إلى مجموعة\n\n_لمزيد من المعلومات، يمكنك إرسال رسالة مباشرة إلى المالك_\n*النوع* \`\`\` .owner\`\`\` *للاتصال بالمالك*`.trim()}, {quoted:m});
+   this.sendMessage(m.chat,{text:`مرحبًا @${m.sender.split('@')[0]}\nيمكنك استئجار الروبوت للانضمام إلى مجموعة\n\n967730068059 _لمزيد من المعلومات، يمكنك إرسال رسالة مباشرة إلى المالك_\n*النوع* \`\`\` .owner\`\`\` *للاتصال بالمالك*`.trim()}, {quoted:m});
    /*this.sendButton(m.chat, `*Invite bot to a group*      
     Hallo @${m.sender.split('@')[0]} 
     you can rent the bot to join a group or contact owner 
