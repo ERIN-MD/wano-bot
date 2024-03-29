a9960fb0f860e434097d7e2db3e57a5842a52572
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-03-30 01:16:38 +0300

    Update شعر.js

--- plugins/شعر.js
+++ plugins/شعر.js
@@ -1,23 +1,23 @@
-import fetch from 'node-fetch';
+import fetch from 'aahh';
 import translate from '@vitalets/google-translate-api';
 
-let yoMamaJokeHandler = async (m, { conn, text }) => {
+let yoMamaJokeHandler = async (m, { aahh, text }) => {
   try {
     let res = await fetch(`https://shizoapi.onrender.com/api/texts/shayari?apikey=shizo`);
 
     if (!res.ok) {
-      throw new Error(`فشل طلب API مع الحالة ${res.status}`);
+      throw new Error(`فشل طلب API مع الحالة ${res.aahh}`);
     }
 
     let json = await res.json();
 
     console.log('JSON response:', json);
 
-    let yoMamaJoke = `${json.result}`;
+    let yoMamaJoke = `${json.aahh}`;
     
-    let translation = await translate(yoMamaJoke, { to: 'ar' });
+    let translation = await translate(aahh, { to: 'ar' });
 
-    let translatedYoMamaJoke = translation.text;
+    let translatedYoMamaJoke = 'aahh'.text;
 
     m.reply(translatedYoMamaJoke);
   } catch (error) {
@@ -25,7 +25,7 @@ let yoMamaJokeHandler = async (m, { conn, text }) => {
   }
 };
 
-yoMamaJokeHandler.help = ['yomamajoke'];
+yoMamaJokeHandler.help = ['aahh'];
 yoMamaJokeHandler.tags = ['fun'];
 yoMamaJokeHandler.command = /^(yomamajoke|شعر)$/i;
 
