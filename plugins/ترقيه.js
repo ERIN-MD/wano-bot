77468222e024848bbf0f9f7b7513b12076bbde86
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-03-31 00:48:38 +0300

    Update ترقيه.js

--- plugins/ترقيه.js
+++ plugins/ترقيه.js
@@ -1,15 +1,15 @@
 let handler = async (m, { conn,usedPrefix, text }) => {
 if(isNaN(text) && !text.match(/@/g)){
-
+	
 }else if(isNaN(text)) {
 var number = text.split`@`[1]
 }else if(!isNaN(text)) {
 var number = text
 }
-
-if(!text && !m.quoted) return conn.reply(m.chat, `*[❗] الاستخدام المناسب*\n\n*┯┷*\n*┠≽ ${usedPrefix}ترقيه مشرف  @منشن*\n*┠≽ ${usedPrefix}ترقيه مشرف  -> الرد على رسالة*\n*┷┯*`, m)
+	
+if(!text && !m.quoted) return conn.reply(m.chat, `*[❗] الاستخدام المناسب*\n\n*┯┷*\n*┠≽ ${usedPrefix}رفع مشرف  @منشن*\n*┠≽ ${usedPrefix}رفع مشرف  -> الرد على رسالة*\n*┷┯*`, m)
 if(number.length > 13 || (number.length < 11 && number.length > 0)) return conn.reply(m.chat, `*[ ⚠️ ] الرقم الذي تم إدخاله غير صحيح ، الرجاء إدخال الرقم الصحيح*`, m)
-
+	
 try {
 if(text) {
 var user = number + '@s.whatsapp.net'
@@ -21,13 +21,13 @@ var user = number + '@s.whatsapp.net'
 } catch (e) {
 } finally {
 conn.groupParticipantsUpdate(m.chat, [user], 'promote')
-conn.reply(m.chat, ``, m)
+conn.reply(m.chat, `*[ ✅ ] تم رفعه مشرف مبروك الادمن يحب ✨💜*`, m)
 }}
-handler.help = ['*201063720595xxx*','*@اسم المستخدم*','*محادثة المستجيب*'].map(v => 'promote ' + v)
+handler.help = ['*201144148194xxx*','*@اسم المستخدم*','*محادثة المستجيب*'].map(v => 'promote ' + v)
 handler.tags = ['group']
-handler.command = /^(ترقيه)$/i
+handler.command = /^(ترقية|ترقيه|رفع)$/i
 handler.group = true
-handler.rowner = true
+handler.admin = true
 handler.botAdmin = true
 handler.fail = null
 export default handler
