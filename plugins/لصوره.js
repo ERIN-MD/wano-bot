fff50dc54f82d60559711c842f4ed3d6e8b52990
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-03-30 04:09:00 +0300

    Create لصوره.js

---
+++ plugins/لصوره.js
@@ -0,0 +1,15 @@
+import {webp2png} from '../lib/webp2mp4.js';
+const handler = async (m, {conn, usedPrefix, command}) => {
+  const notStickerMessage = `*««لـصـوره»»*\n قم بالرد علي الملصق اللي عايز تحولوا لصوره*`;
+  if (!m.quoted) throw notStickerMessage;
+  const q = m.quoted || m;
+  const mime = q.mediaType || '';
+  if (!/sticker/.test(mime)) throw notStickerMessage;
+  const media = await q.download();
+  const out = await webp2png(media).catch((_) => null) || Buffer.alloc(0);
+  await conn.sendFile(m.chat, out, 'error.png', null, m);
+};
+handler.help = ['toimg (reply)'];
+handler.tags = ['sticker'];
+handler.command = ['لصوره', 'jpg', 'img'];
+export default handler;
