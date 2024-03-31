91d5222a7afc749da1dd6f2dca389fb002f23b80
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-03-31 03:57:59 +0300

    Update لانمي.js

--- plugins/لانمي.js
+++ plugins/لانمي.js
@@ -1,29 +1,27 @@
-import uploadImage from '../lib/uploadImage.js';
-const handler = async (m, {conn, text, args, usedPrefix, command}) => {
-  const q = m.quoted ? m.quoted : m;
-  const mime = (q.msg || q).mimetype || q.mediaType || '';
-  if (!/image/g.test(mime)) throw '*[❗] فين الصوره الي انت عايزها بشكل انمي*';
-  m.reply('*[❗] يتم تحويل الصوره لانمي، خلي عندك صبر عشان النتيجه*');
-  const data = await q.download?.();
-  const image = await uploadImage(data);
-  try {
-    const anime = `https://api.lolhuman.xyz/api/imagetoanime?apikey=${lolkeysapi}&img=${image}`;
-    await conn.sendFile(m.chat, anime, 'error.jpg', null, m);
-  } catch (i) {
-    try {
-      const anime2 = `https://api.zahwazein.xyz/photoeditor/jadianime?url=${image}&apikey=${keysxxx}`;
-      await conn.sendFile(m.chat, anime2, 'error.jpg', null, m);
-    } catch (a) {
-      try {
-        const anime3 = `https://api.caliph.biz.id/api/animeai?img=${image}&apikey=caliphkey`;
-        await conn.sendFile(m.chat, anime3, 'error.jpg', null, m);
-      } catch (e) {
-        throw '*[❗] خطا، وجه الشخص غير واضح*';
-      }
-    }
-  }
-};
-handler.help = ['toanime'];
-handler.tags = ['tools'];
-handler.command = /^(jadianime|لانمي)$/i;
-export default handler;
+import uploadImage from  '../lib/uploadImage.js'
+let handler = async (m, { conn, text, args, usedPrefix, command }) => {
+let q = m.quoted ? m.quoted : m
+let mime = (q.msg || q).mimetype || q.mediaType || ""
+if (!/image/g.test(mime)) throw  '*Respond to a image* '
+m.reply('*تحويل صورة لانمي* \n ثانية من وقتك تابع صاحب البوت في حسابه حتى تكون اول من يصله جديد البوتات و صاحب افضل بوتات عربية \n https://solo.to/shadowsensei ')    
+let data = await q.download?.()
+let image = await uploadImage(data)
+try {
+let anime = `https://api.lolhuman.xyz/api/imagetoanime?apikey=${lolkeysapi}&img=${image}`
+await conn.sendFile(m.chat, anime,  'error.jpg' , null, m)
+} catch (i) {
+try {
+let anime2 = `https://api.zahwazein.xyz/photoeditor/jadianime?url=${image}&apikey=${keysxxx}`
+await conn.sendFile(m.chat, anime2,  'error.jpg' , null, m) 
+} catch (a) {    
+try{    
+let anime3 = `https://api.caliph.biz.id/api/animeai?img=${image}&apikey=caliphkey`
+await conn.sendFile(m.chat, anime3,  'error.jpg' , null, m) 
+} catch (e) {
+throw '*الصورة التي ارسلتها غير واضحة ارسل صورة يكون فيها الوجه واضحا*'
+}}}}
+handler.help = ["toanime"]
+handler.tags = ["tools"]
+handler.diamond = true
+handler.command = /^(لانمي|لأنمي|لانيمي|toanime)$/i
+export default handler
