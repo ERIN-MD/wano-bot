841b6e9a56781adbaac96c86d09f89a7624e86a1
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-04-01 11:51:04 +0300

    Update للصوت.js

--- plugins/للصوت.js
+++ plugins/للصوت.js
@@ -1,7 +1,7 @@
 // كود تم نشره بواسطه obito
 // تابع لقناه https://whatsapp.com/channel/0029VaSQX1TI1rcbxtQZ5518
 let handler = async (m, { conn }) => {
-let done = '🩶'; 
+let done = 'dhdhhs'; 
        m.react(done);
     let user = global.db.data.users[m.sender];
     let name = conn.getName(m.sender);
@@ -11,7 +11,7 @@ let done = '🩶';
     conn.sendFile(m.chat, 'تحط الصوره هنا تليجرام', 'image.jpg', message, m);
 };
 
-handler.customPrefix = /^(bot|بوت)$/i;
+handler.customPrefix = /^(لصوت2|لصوت2)$/i;
 handler.command = new RegExp;
 
 export default handler;
