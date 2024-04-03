66265e26d22cd8d3e33d7a1c214a281fd715ce0c
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-04-03 07:51:29 +0300

    Update تطقيم.js

--- plugins/تطقيم.js
+++ plugins/تطقيم.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch"
 let handler = async (m, { conn }) => {
 
-  let data = await (await fetch('https://raw.githubusercontent.com/KazukoGans/database/main/anime/ppcouple.json')).json()
+  let data = await (await fetch('https://gist.githubusercontent.com/kdjrjidj/9d3ab3158d5c42934102969a6a854df4/raw/b77218ccda12fb5cd0c69ec735cc6f9d474cce7e/xjsjjdjdjsd.json')).json()
   let cita = data[Math.floor(Math.random() * data.length)]
   
   let cowi = await(await fetch(cita.cowo)).buffer()
