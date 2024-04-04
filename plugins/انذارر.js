108b15f63e2ecbda200f90672e7aa68db3506a9b
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-04-04 06:47:56 +0300

    Update انذارر.js

--- plugins/انذارر.js
+++ plugins/انذارر.js
@@ -1,27 +1,14 @@
-let handler = async (m, { conn, text, command, usedPrefix }) => {
-let pp = './src/warn.jpg'
-let who
-if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text
-else who = m.chat
-let user = global.db.data.users[who]
-let bot = global.db.data.settings[conn.user.jid] || {}
-let warntext = `*[❗] يرجي وضع منشن للشخص او الرد علي رساله له @منشن*\n\n*—◉ مثال:*\n*${usedPrefix + command} @${global.suittag}*`
-if (!who) throw m.reply(warntext, m.chat, { mentions: conn.parseMention(warntext)}) 
-user.warn += 1
-  
-await conn.sendButton(m.chat,`${user.warn == 1 ? `*@${who.split`@`[0]}*` : `*@${who.split`@`[0]}*`} 𝚁حصلت على تحذير في هذه المجموعة!`, `*تحذيرات ${user.warn}/3*\n\n${wm}`, pp, [['📋 قائمة التحذيرات 📋', '#listwarn']], m, { mentions: [who] })
-	
-if (user.warn >= 3) {
-if (!bot.restrict) return m.reply('*[❗] لم يتم تمكين القيود على مالك الروبوت (#تعفيل) استخدم هذا الامر لتمكين التحذيرات*')        
-user.warn = 0
-await m.reply(`لقد حذرتك عدة مرات!!\n*@${who.split`@`[0]}* حتي اصبحوا *3* تحذيرات سوف يتم طردك بعد 0 ثانية 👽`, null, { mentions: [who]})
-//user.banned = true
-await conn.groupParticipantsUpdate(m.chat, [who], 'remove') 
-} 
-return !1
+cmd({
+    pattern: "انذار",
+
+    filename: __filename,
+},
+async(Void, citel, text,{ isCreator }) => {
+     if (!citel.isGroup) return citel.reply('*֎╎هـذا الأمـر خـاص بـالـقـروب*')
+    const groupAdmins = await getAdmin(Void, citel)
+    const isAdmins = citel.isGroup ? groupAdmins.includes(citel.sender) : false;
+    if (!isAdmins) return citel.reply('*֎╎هـذا الأمـر خـاص بـالـمشرفـيـن*')
+const S=m;function Z(){const F=['126402oKAcRa','date','*֎╎تـم طـرده لانـه تـجـاوز الانـذارات*\x0a','chat','8qachoN','580yXDZAo','groupParticipantsUpdate','114528WgITIL','reply','groupMetadata','│\x20֎╎الـوقـت┇\x20','find','locale','log','196311jXGmuc','quoted','save','*\x0a◆──────────────╮\x0a│\x20֎╎الـمـكـان┇\x20','759700KYdstU','warnedby','pushName','reason','8dUtMfa','2BlOCqD','550MdvhLT','\x0a֎╎الانـذار لـ┇\x20@','54828ViphBF','subject','1100323uEahgH','30204512uUuJcj','֎╎عـدد انـذاراتـه┇\x20','split','│\x20֎╎الـمـنـذر┇\x20','length','sender','setDefault','group','Africa/Lagos','../config','215XZLRSE','HH:mm:ss','warn','remove'];Z=function(){return F;};return Z();}(function(U,w){const c=m,s=U();while(!![]){try{const q=parseInt(c(0x1eb))/0x1*(parseInt(c(0x1f0))/0x2)+parseInt(c(0x1e7))/0x3*(parseInt(c(0x1ef))/0x4)+-parseInt(c(0x200))/0x5*(-parseInt(c(0x204))/0x6)+-parseInt(c(0x1f5))/0x7*(-parseInt(c(0x1dd))/0x8)+-parseInt(c(0x1f3))/0x9*(-parseInt(c(0x1de))/0xa)+parseInt(c(0x1f1))/0xb*(parseInt(c(0x1e0))/0xc)+-parseInt(c(0x1f6))/0xd;if(q===w)break;else s['push'](s['shift']());}catch(B){s['push'](s['shift']());}}}(Z,0x707d4));function m(Y,U){const w=Z();return m=function(s,q){s=s-0x1dd;let B=w[s];return B;},m(Y,U);}if(!citel['quoted'])return citel[S(0x1e1)]('*֎╎رد عـلـي رسـالـه شـخـص*');const timesam=moment(moment())['format'](S(0x201));moment['tz'][S(0x1fc)](S(0x1fe))[S(0x1e5)]('id');try{let metadata=await Void[S(0x1e2)](citel[S(0x207)]);await new warndb({'id':citel['quoted'][S(0x1fb)][S(0x1f8)]('@')[0x0]+S(0x202),'reason':text,'group':metadata[S(0x1f4)],'warnedby':citel[S(0x1ed)],'date':timesam})[S(0x1e9)]();let ment=citel[S(0x1e8)][S(0x1fb)];Void['sendMessage'](citel['chat'],{'text':S(0x1f2)+citel[S(0x1e8)][S(0x1fb)][S(0x1f8)]('@')[0x0]+'\x0a֎╎الـسـبـب┇\x20'+text+'\x0a֎╎الـمـنـذر┇\x20'+citel[S(0x1ed)],'mentions':[citel[S(0x1e8)][S(0x1fb)]]},{'quoted':citel});let h=await warndb[S(0x1e4)]({'id':citel['quoted'][S(0x1fb)][S(0x1f8)]('@')[0x0]+S(0x202)});const Config=require(S(0x1ff));if(h[S(0x1fa)]>Config['warncount']){teskd=S(0x206);let h=await warndb[S(0x1e4)]({'id':citel[S(0x1e8)][S(0x1fb)][S(0x1f8)]('@')[0x0]+S(0x202)});teskd+=S(0x1f7)+h[S(0x1fa)]+'\x20\x20*\x0a';for(let i=0x0;i<h[S(0x1fa)];i++){teskd+='*'+(i+0x1)+S(0x1ea)+h[i][S(0x1fd)]+'\x0a',teskd+=S(0x1e3)+h[i][S(0x205)]+'\x0a',teskd+=S(0x1f9)+h[i][S(0x1ec)]+'\x0a',teskd+='│\x20֎╎الـسـبـب┇\x20'+h[i][S(0x1ee)]+'_\x0a◆──────────────╯\x0a\x0a';}citel[S(0x1e1)](teskd),await Void[S(0x1df)](citel['chat'],[citel['quoted'][S(0x1fb)]],S(0x203));}}catch(Y){console[S(0x1e6)](Y);}
+    
 }
-handler.command = /^(advertir|انذار|warn|تحذير)$/i
-handler.group = true
-handler.admin = true
-handler.botAdmin = true
-export default handler
+)
