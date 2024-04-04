4b4d97abee88726ca6ef380f8eed20814e9a1689
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-04-04 09:42:20 +0300

    Update احزر2.js

--- plugins/احزر2.js
+++ plugins/احزر2.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn, command, usedPrefix }) => {
         conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tebakbendera[id][0])
         throw false
     }
-    let src = await (await fetch('https://gist.githubusercontent.com/kdjrjidj/ecb7f328b56c021dc7462dbadca50c9c/raw/5aafea1eb85f64eb3c52dc37d527038ab8dc5bf7/djhjhxjdjdjbjerj.txtjdjdjbjerj.txtxt')).json()
+    let src = await (await fetch('https://gist.githubusercontent.com/kdjrjidj/ecb7f328b56c021dc7462dbadca50c9c/raw/50e9b64a76beb90c2f7792ee4614cb5e58b0bcae/djhjhxjdjdjbjerj.txt')).json()
   let json = src[Math.floor(Math.random() * src.length)]
     let caption = `*${command.toUpperCase()}*
   ❐↞┇الـوقـت⏳↞ *${(timeout / 1000).toFixed(2)} ┇
