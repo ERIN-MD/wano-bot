913e0ffa8989747b2378da956c01fc5480a9bd9f
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-04-05 17:14:25 +0300

    Update خلفياتت.js

--- plugins/خلفياتت.js
+++ plugins/خلفياتت.js
@@ -15,7 +15,7 @@ let link = img
 }
 handler.help = ['', '2'].map(v => 'wallpaper' + v + ' <query>')
 handler.tags = ['downloader']
-handler.command = /^(wp|خلفية|خلفيه|wallpaper2?)$/i
+handler.command = /^خلفيه$/i
 handler.exp = 29
 handler.limit = 2
 handler.register = true
