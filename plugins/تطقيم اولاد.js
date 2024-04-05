715419f6eef7ac46e729877a1592c9ae92bbbcb8
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-04-05 16:30:32 +0300

    Update تطقيم اولاد.js

--- plugins/تطقيم اولاد.js
+++ plugins/تطقيم اولاد.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch"
 let handler = async (m, { conn }) => {
 
-  let data = await (await fetch('https://gist.githubusercontent.com/kdjrjidj/9d3ab3158d5c42934102969a6a854df4/raw/4fe0cb9c04d00d9b630b0e7a47b82cee46e90db0/xjsjjdjdjsd.jsonn')).json()
+  let data = await (await fetch('https://gist.githubusercontent.com/kdjrjidj/9d3ab3158d5c42934102969a6a854df4/raw/4fe0cb9c04d00d9b630b0e7a47b82cee46e90db0/xjsjjdjdjsd.json')).json()
   let cita = data[Math.floor(Math.random() * data.length)]
   
   let cowi = await(await fetch(cita.sjdbs)).buffer()
