7510ba146caf2e13c1344afa036cbb993fee9b89
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-04-05 15:49:44 +0300

    Update تطقيم.js

--- plugins/تطقيم.js
+++ plugins/تطقيم.js
@@ -1,7 +1,7 @@
 import fetch from "node-fetch"
 let handler = async (m, { conn }) => {
 
-  let data = await (await fetch('https://gist.githubusercontent.com/kdjrjidj/9d3ab3158d5c42934102969a6a854df4/raw/9dc72b1ce4348bf8e30593d74f4e60d6cf6e33d2/xjsjjdjdjsd.jsonn')).json()
+  let data = await (await fetch('https://gist.githubusercontent.com/kdjrjidj/9d3ab3158d5c42934102969a6a854df4/raw/9dc72b1ce4348bf8e30593d74f4e60d6cf6e33d2/xjsjjdjdjsd.json')).json()
   let cita = data[Math.floor(Math.random() * data.length)]
   
   let cowi = await(await fetch(cita.sjdbs)).buffer()
@@ -11,7 +11,7 @@ let handler = async (m, { conn }) => {
 }
 handler.help = ['ppcouple', 'ppcp']
 handler.tags = ['internet']
-handler.command = ['تطقيم','طقم'] 
+handler.command = ['تطقيم-اولاد','طقم-اولاد'] 
 
 
 export default handler
