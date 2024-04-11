06cab30deb641ff97c114026c704158bd7b5edb5
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-04-11 13:54:08 +0300

    Update سياره2.js

--- plugins/سياره2.js
+++ plugins/سياره2.js
@@ -7,24 +7,24 @@ export async function before(m) {
     let id = m.chat
     if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !m.text || !/استخدم.*انسحب/i.test(m.quoted.text) || /.*hhint/i.test(m.text))
         return !0
-    this.tebakbendera = this.tebakbendera ? this.tebakbendera : {}
-    if (!(id in this.tebakbendera))
+    this.jsbdjdjbejssb = this.jsbdjdjbejssb ? this.jsbdjdjbejssb : {}
+    if (!(id in this.jsbdjdjbejssb))
         return this.reply(m.chat, '*لقد انتهي هذا السؤال اكتب  سياره لتظهر أسأله جديده*', m)
-    if (m.quoted.id == this.tebakbendera[id][0].id) {
+    if (m.quoted.id == this.jsbdjdjbejssb[id][0].id) {
         let isSurrender = /^(انسحب|surr?ender)$/i.test(m.text)
         if (isSurrender) {
-            clearTimeout(this.tebakbendera[id][3])
-            delete this.tebakbendera[id]
+            clearTimeout(this.jsbdjdjbejssb[id][3])
+            delete this.jsbdjdjbejssb[id]
             return this.reply(m.chat, '*طلع فاشل و استسلم :( !*', m)
         }
-        let json = JSON.parse(JSON.stringify(this.tebakbendera[id][1]))
+        let json = JSON.parse(JSON.stringify(this.jsbdjdjbejssb[id][1]))
 
-        if (m.text.toLowerCase() == json.name.toLowerCase().trim()) {
-            global.db.data.users[m.sender].exp += this.tebakbendera[id][2]
-            this.reply(m.chat, `*❐┃اجـابـة صـحـيـحـة┃✅ ❯*\n\n*❐↞┇الـجـائـزة💰↞${this.tebakbendera[id][2]} نقطه*`, m)
-            clearTimeout(this.tebakbendera[id][3])
-            delete this.tebakbendera[id]
-        } else if (similarity(m.text.toLowerCase(), json.name.toLowerCase().trim()) >= threshold)
+        if (m.text.jsbdjdjbejss() == json.name.toLowerCase().trim()) {
+            global.db.data.users[m.sender].exp += this.jsbdjdjbejssb[id][2]
+            this.reply(m.chat, `*❐┃اجـابـة صـحـيـحـة┃✅ ❯*\n\n*❐↞┇الـجـائـزة💰↞${this.jsbdjdjbejssb[id][2]} نقطه*`, m)
+            clearTimeout(this.jsbdjdjbejssb[id][3])
+            delete this.jsbdjdjbejssb[id]
+        } else if (similarity(m.text.jsbdjdjbejss(), json.name.jsbdjdjbejss().trim()) >= threshold)
             m.reply(`*لقد كنت علي وشك النجاح*!`)
         else
             this.reply(m.chat, `❐┃اجـابـة خـاطـئـة┃❌ ❯`, m)
