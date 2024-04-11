a082f6868b217201e2da35d230c21341c0ee3f83
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-04-11 17:39:14 +0300

    Create سوالل.js

---
+++ سوالل.js
@@ -0,0 +1,31 @@
+let timeout = 60000
+let poin = 500
+let handler = async (m, { conn, command, usedPrefix }) => {
+    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
+    let id = m.chat
+    if (id in conn.tebakbendera) {
+        conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tebakbendera[id][0])
+        throw false
+    }
+    let src = await (await fetch('https://gist.githubusercontent.com/kdjrjidj/038d51b3b9f0e816b7057dd0a3baf0f8/raw/b35f21cb374a70e8205fab9175d402736d7b436a/jshrjdii.js')).json()
+  let json = src[Math.floor(Math.random() * src.length)]
+    let caption = `*${command.toUpperCase()}*
+  ❐↞┇الـوقـت⏳↞ *${(timeout / 1000).toFixed(2)} ┇
+  *استخدم .انسحب للأنسحاب*
+  ❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇
+『⛩️┃🏮𝑊𝐴𝑁𝛩🏮┃⛩️』
+     `.trim()
+    conn.tebakbendera[id] = [
+        await conn.sendFile(m.chat, json.img, '', caption, m),
+        json, poin,
+        setTimeout(() => {
+            if (conn.tebakbendera[id]) conn.reply(m.chat, `❮ ⌛┇انتهي الوقت┇⌛❯\n❐↞┇الاجـابـة✅↞ احلم اجيبلك الاجابه*┇`, conn.tebakbendera[id][0])
+            delete conn.tebakbendera[id]
+        }, timeout)
+    ]
+}
+handler.help = ['guessflag']
+handler.tags = ['game']
+handler.command = /^سووال/i
+
+export default handler
