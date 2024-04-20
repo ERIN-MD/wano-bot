4557d0786d6ec2e0d300feb89ad0ecdbe0d69339
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-04-20 15:56:58 +0300

    Update تطقيم.js

--- plugins/تطقيم.js
+++ plugins/تطقيم.js
@@ -4,10 +4,10 @@ let handler = async (m, { conn }) => {
   let data = await (await fetch('https://gist.githubusercontent.com/kdjrjidj/58200892612d853c879660227d600445/raw/fb9be0a9994dff6edb6fa5490e34023b954dbada/xxxxha.js')).json()
   let cita = data[Math.floor(Math.random() * data.length)]
   
-  let cowi = await(await fetch(cita.sjdbs)).buffer()
-  await conn.sendFile(m.chat, cowi, '⛩️┃🏮𝑊𝐴𝑁𝛩🏮┃⛩️', '👤', m)
-  let ciwi = await(await fetch(cita.hsvqwiw)).buffer()
-  await conn.sendFile(m.chat, ciwi, '⛩️┃🏮𝑊𝐴𝑁𝛩🏮┃⛩️', '👤', m)
+  let cowi = await(await fetch(cita.cowo)).buffer()
+  await conn.sendFile(m.chat, cowi, '⛩️┃🏮𝑊𝐴𝑁𝛩🏮┃⛩️', '🙎‍♂️', m)
+  let ciwi = await(await fetch(cita.cewe)).buffer()
+  await conn.sendFile(m.chat, ciwi, '⛩️┃🏮𝑊𝐴𝑁𝛩🏮┃⛩️', '🙎‍♀️', m)
 }
 handler.help = ['ppcouple', 'ppcp']
 handler.tags = ['internet']
