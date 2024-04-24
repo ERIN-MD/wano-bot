0b8972907151fb52d15b7fcdad59a75b6b37fc19
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-04-24 17:16:12 +0300

    Update سياره.js

--- plugins/سياره.js
+++ plugins/سياره.js
@@ -4,26 +4,26 @@
 let timeout = 60000
 let poin = 500
 let handler = async (m, { conn, command, usedPrefix }) => {
-    conn.jsbdjdjbejssb = conn.jsbdjdjbejssb ? conn.jsbdjdjbejssb : {}
+    conn.tebakbendera = conn.tebakbendera ? conn.tebakbendera : {}
     let id = m.chat
-    if (id in conn.jsbdjdjbejssb) {
-        conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.jsbdjdjbejssb[id][0])
+    if (id in conn.tebakbendera) {
+        conn.reply(m.chat, '❐┃لم يتم الاجابة علي السؤال بعد┃❌ ❯', conn.tebakbendera[id][0])
         throw false
     }
     let src = await (await fetch('https://gist.githubusercontent.com/ggbot564/de6999400bb50eecd7b5f65f67f66786/raw/dd9a5a3cf73fc6bd67c04b3f1a88eea0f049f1db/cars')).json()
   let json = src[Math.floor(Math.random() * src.length)]
-    let caption = `*${command.jsbdjdjbejss()}*
+    let caption = `*${command.toUpperCase()}*
   ❐↞┇الـوقـت⏳↞ *${(timeout / 1000).toFixed(2)} ┇
  استخدم .انسحب للأنسحاب
   ❐↞┇الـجـائـزة💰↞ ${poin} نقاط┇
-『⛩️┃𝑊𝛩𝑁𝛩『 🏮 』𝐵𝛩𝑇┃⛩️』
+『killua-bot』
      `.trim()
-    conn.jsbdjdjbejssb[id] = [
+    conn.tebakbendera[id] = [
         await conn.sendFile(m.chat, json.img, '', caption, m),
         json, poin,
         setTimeout(() => {
-            if (conn.jsbdjdjbejssb[id]) conn.reply(m.chat, `❮ ⌛┇انتهي الوقت┇⌛❯\n❐↞┇الاجـابـة✅↞ ${json.name}*┇`, conn.jsbdjdjbejssb[id][0])
-            delete conn.jsbdjdjbejssb[id]
+            if (conn.tebakbendera[id]) conn.reply(m.chat, `❮ ⌛┇انتهي الوقت┇⌛❯\n❐↞┇الاجـابـة✅↞ ${json.name}*┇`, conn.tebakbendera[id][0])
+            delete conn.tebakbendera[id]
         }, timeout)
     ]
 }
