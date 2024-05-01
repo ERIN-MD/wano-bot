ded0c3b01f6e24741065aee5a276d39ee92401ec
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-05-01 12:51:06 +0300

    Update الازرار.js

--- plugins/الازرار.js
+++ plugins/الازرار.js
@@ -1,42 +1,46 @@
+حقوق  - _( 🍷 ڤينوم و سوكونا ☘️ )_ 
+> _*كود الزراير الجديد تم صنعه بواسطه ڤينوم و سوكونا*_
+
+*(🎧) شارك رابط القناه ادعمنا لي تنزيل اكواد اخري*
+_https://whatsapp.com/channel/0029VaQim2bAu3aPsRVaDq3v_
+
+
+*(⚙️)-) الكود (-(⚙️)*
+
+
 let handler = async (m, { conn, args, usedPrefix, command }) => {
     conn.relayMessage(m.chat, {
       viewOnceMessage: {
         message: {
           interactiveMessage: {
             header: {
-              title: 'الازرار'
+              title: '*🎗️ قـائـمـة الـاوامــر🎗️*'
             },
             body: {
-              text: 'تست'
+              text: '🧿 افتح القائمة بواسطة الزر\n🍒 لا تلعب كثير في القائمة'
             },
             nativeFlowMessage: {
               buttons: [
                 {
                   name: 'single_select',
                   buttonParamsJson: JSON.stringify({
-                    title: 'اضغط',
+                    title: 'دوس عليا 💔',
                     sections: [
                       {
-                        title: 'القائمة',
-                        highlight_label: 'تست',
+                        title: 'List',
+                        highlight_label: 'ON',
                         rows: [
                           {
-                            header: 'الاوامر',
-                            title: '',
+                            header: '☘️ قـسـم الـنـظـام',
+                            title: '.النظام_كود',
                             description: '',
-                            id: 'اوامر.'
-                          },
-                                        {
-                            header: 'المطور',
-                            title: '',
-                            description: '',
-                            id: '.المطور.'
+                            id: 'te'
                           },
                           {
-                            header: 'المطور',
-                            title: 'المطور',
+                            header: '👑 قـسـم الـمـطـور',
+                            title: '.المطور_كود',
                             description: '',
-                            id: '.مطور'
+                            id: 'te'
                           }
                         ]
                       }
