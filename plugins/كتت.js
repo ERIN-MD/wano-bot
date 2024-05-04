9020ce412da58911dbeece53889c298eb54331a4
kdjrjidj <164735936+kdjrjidj@users.noreply.github.com>
2024-05-04 23:17:39 +0300

    Update كتت.js

--- plugins/كتت.js
+++ plugins/كتت.js
@@ -3,7 +3,7 @@ let handler = m => m;
  handler.all = async function (m) { 
    let chat = global.db.data.chats[m.chat]; 
    let responses; 
-   if (/^كت$/i;.test(m.text))
+   if (/^كت$/i.test(m.text)) { 
      responses = [ 
                     `*∞──────「كـت」──────∞*
 
